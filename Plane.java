3ca86a141c1b7242a5985badce9cec7096c190f5
overactor <rik.degraaff@gmx.ch>
2014-07-28 16:18:36 +0200

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -3,31 +3,46 @@ package Planes;
 //Objects of this class contain all relevant information about a plane
 //as well as some helper functions.
 public class Plane {
-	private int x, y, z;
+	private Point3D position;
 	private Direction direction;
 	private int arenaSize;
 	private boolean alive = true;
 	private int coolDown = 0;
  
-	public Plane(int arenaSize) {
+	public Plane(int arenaSize, Direction direction, int x, int y, int z) {
 		this.arenaSize = arenaSize;
+		this.position = new Point3D(x, y, z);
+		alive = position.isInArena(arenaSize);
+		this.direction = direction;
+	}
+	 
+	public Plane(int arenaSize, Direction direction, Point3D position) {
+		this.arenaSize = arenaSize;
+		this.position = new Point3D(position.x, position.y, position.z);
+		alive = position.isInArena(arenaSize);
+		this.direction = direction;
 	}    
  
 	// Returns the x coordinate of the plane
 	public int getX() {
-		return (alive)?x:-1;
+		return (alive)?position.x:-1;
 	}
  
 	// Returns the y coordinate of the plane
 	public int getY() {
-		return (alive)?y:-1;
+		return (alive)?position.y:-1;
 	}
-	 
+
 	// Returns the z coordinate of the plane
 	public int getZ() {
-		return (alive)?z:-1;
+		return (alive)?position.z:-1;
 	}
-	 
+	
+	// Returns the position as a Point3D.
+	public Point3D getPosition() {
+		return position;
+	}
+
 	// Returns the distance between the plane and the specified wall,
 	// 0 means right next to it, 19 means at the opposite side.
 	// Returns -1 for invalid input.
@@ -35,17 +50,17 @@ public class Plane {
 	    if (alive) {
 	        switch (wall) {
 	            case 'N':
-	                return x;
+	                return position.x;
 	            case 'S':
-	                return arenaSize - x - 1;
+	                return arenaSize - position.x - 1;
 	            case 'W':
-	                return y;
+	                return position.y;
 	            case 'E':
-	                return arenaSize - y - 1;
+	                return arenaSize - position.y - 1;
 	            case 'D':
-	                return z;
+	                return position.z;
 	            case 'U':
-	                return arenaSize - z - 1;
+	                return arenaSize - position.z - 1;
 	            default:
 	                return -1;
 	        }
@@ -53,7 +68,7 @@ public class Plane {
 	        return -1;
 	    }
 	}
-	 
+
 	// Returns the direction of the plane.
 	public Direction getDirection() {
 	    if (alive) {
@@ -62,7 +77,7 @@ public class Plane {
 	        return null;
 	    }
 	}
-	 
+
 	// Returns all possible turning directions for the plane.
 	public Direction[] getPossibleDirections() {
 	    if (alive) {
@@ -71,37 +86,96 @@ public class Plane {
 	        return null;
 	    }        
 	}
-	 
+
 	// Returns the cool down before the plane will be able to shoot, 
 	// 0 means it is ready to shoot this turn.
 	public int getCoolDown() {
 	    return coolDown;
 	}
-	 
+	
+	public void setCoolDown(int coolDown) {
+		this.coolDown = coolDown;
+	}
+
 	// Returns true if the plane is ready to shoot
 	public boolean canShoot() {
-	    return coolDown == 0;
+	    return coolDown == 0 && alive;
 	}
-	 
+
 	// Returns all positions this plane can shoot at (without first making a move).
-	// Not done yet.
-	public Point3D[] getShootRange() {
-		return null;
-	     
+	public Point3D[] getShootRange() {		
+		if (alive) {
+			int maxDistance = 0;
+			
+			for (int i=0; i<direction.getMainDirections().length; i++) {
+				maxDistance = Math.max(maxDistance, getDistanceFromWall(direction.getMainDirections()[i]));
+			}
+			
+			Point3D[] range = new Point3D[maxDistance];
+			
+			for (int i=1; i<maxDistance; i++) {
+				range[i] = position.add(direction.getAsPoint3D().multiply(i));
+			}
+			
+			return range;
+		} else {
+			return null;
+		}
 	}
-	 
+
 	// Returns all positions this plane can move to within one turn.
-	// Not done yet.
 	public Point3D[] getRange() {
-		return null;
-	     
+		if (alive) {
+			Direction[] directions = getPossibleDirections();
+			Point3D[] range = new Point3D[directions.length];
+			
+			for (int i=0; i<directions.length; i++) {
+				range[i] = position.add(directions[i].getAsPoint3D());
+			}
+			
+			return range;
+		} else {
+			return null;
+		}
+		
 	}
-	 
+
 	// Returns a plane that represents this plane after making a certain move,
 	// not taking into account other planes.
-	// Not done yet.
 	public Plane simulateMove(Move move) {
-	return null;
-	     
-	} 
+		if (!alive) {
+			return copy();
+		}
+		
+		Direction newDirection;
+		
+		if (!direction.isValidDirection(move.direction)) {
+			move.direction = direction; // If a direction is invalid, you fly straight ahead.
+		}
+		
+		if (move.changeDirection) {
+			newDirection = move.direction;
+		} else {
+			newDirection = direction;
+		}
+		
+		return new Plane(arenaSize, newDirection, position.add(move.direction.getAsPoint3D()));
+	}
+	
+	// Returns true if the plane is alive.
+	public boolean isAlive() {
+		return alive;
+	}
+	
+	// Sets alive to the specified value.
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+	
+	// returns a copy of itself.
+	public Plane copy() {
+		Plane copyPlane = new Plane(arenaSize, new Direction(direction.getNSDir(), direction.getWEDir(), direction.getDUDir()), new Point3D(position.x, position.y, position.z));
+		copyPlane.setAlive(alive);
+		return copyPlane;
+	}
 }
