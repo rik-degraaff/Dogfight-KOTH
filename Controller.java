6e44e0da1c2edfdb88f8eaca7cc651a5d0f7d7ac
overactor <rik.degraaff@gmx.ch>
2014-07-30 08:05:54 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -8,27 +8,27 @@ public class Controller {
 	private static final int ARENA_SIZE = 14;
 	private static final int ROUNDS = 100;
 	private static final int FIGHTS = 10;
-	private static final int COOLDOWN = 1;
+	public static final int COOLDOWN = 1;
 
 	public static void main(String []args){
-		
+
 		// This is a mock tournament between three DumbPlanes, replace these to test your own entries.
 		PlaneControl[] entries = {new DumbPlanes(ARENA_SIZE, ROUNDS), new DumbPlanes(ARENA_SIZE, ROUNDS), new DumbPlanes(ARENA_SIZE, ROUNDS)}; // Instances of all entries!!!
 		String[] names = {"DumbPlanes1", "DumbPlanes2", "DumbPlanes3"}; // corresponding names of all entries!!!
 		int[] scores = new int[entries.length];
-		
+
 		for (int i=0; i<scores.length; i++) {
 			scores[i] = 0;
 		}
-		
+
 		try {
 			PrintWriter out = new PrintWriter("test.txt");
 			String eol = System.getProperty("line.separator");
-			
+
 			out.println("THE TOURNAMENT BEGINS!" + eol + eol);
-			
+
 			out.println(eol + "----------------------------------------------------------------------------------------------------------------------------------------------------------------" + eol);
-		
+
 			//matchUp(new DumbPlanes(ARENA_SIZE, ROUNDS), "player1", new DumbPlanes(ARENA_SIZE, ROUNDS), "player2", out); // Use this to simulate a quick matchup between two planes.
 			for (int i=0; i<entries.length - 1; i++) {
 				for (int j=i+1; j<entries.length; j++) {
@@ -38,12 +38,12 @@ public class Controller {
 					scores[j] += (winner==-1)?2:(winner==1)?0:1;
 				}
 			}
-			
+
 			int topScore = 0;
 			String topScorer = "";
-			
+
 			out.println(eol + "SCORES:" + eol);
-			
+
 			for (int i=0; i<scores.length; i++) {
 				if (((scores[i] == topScore) && (topScorer == "")) || (scores[i] > topScore)) {
 					topScorer = names[i];
@@ -51,44 +51,44 @@ public class Controller {
 				} else if (scores[i] == topScore) {
 					topScorer = topScorer + " and " + names[i];
 				}
-				
+
 				out.println(names[i] + ": " + scores[i] + " points.");
 			}
-			
+
 			out.println(eol + eol + "THE OVERALL WINNER(S): " + topScorer + eol + "With " + Integer.toString(topScore) + " points.");
-			
+
 			out.close();
-		
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
     }
-	
+
 	// 0 means draw, 1 means player one won, -1 means player2 won.
 	private static int matchUp(PlaneControl player1, String player1String, PlaneControl player2, String player2String, PrintWriter out) {
 		int player1Score = 0;
 		int player2Score = 0;
-		
+
 		String eol = System.getProperty("line.separator");
 
 		player1.newOpponent(FIGHTS);
 		player2.newOpponent(FIGHTS);
-		
+
 		out.println("The match between " + player1String + " and " + player2String + " begins and will go on for " + FIGHTS + " fights." + eol);
-		
+
 		for (int i=0; i<FIGHTS; i++) {
 			player1.newFight(i, player1Score, player2Score);
 			player2.newFight(i, player2Score, player1Score);
-			
+
 			out.println(eol + "FIGHT " + Integer.toString(i + 1) + eol);
-			
+
 			int winner = fight(player1, player1String, player2, player2String, out);
 			player1Score += (winner==1)?1:0;
 			player2Score += (winner==-1)?1:0;
-			
+
 			out.println("SCORE: " + player1String + ": " + Integer.toString(player1Score) + " " + player2String + ": " + Integer.toString(player2Score) + eol);
 		}
-		
+
 		if (player1Score > player2Score) {
 			out.println(player1String + " WINS!!!" + eol);
 		} else if (player1Score < player2Score) {
@@ -96,10 +96,10 @@ public class Controller {
 		} else {
 			out.println("IT'S A DRAW!" + eol);
 		}
-		
+
 		return (player1Score > player2Score)?1:(player1Score == player2Score)?0:-1;
 	}
-	
+
 	// 0 means draw, 1 means player one won, -1 means player2 won. 
 	private static int fight(PlaneControl player1, String player1String, PlaneControl player2, String player2String, PrintWriter out) {
 
@@ -107,14 +107,14 @@ public class Controller {
 								new Plane(ARENA_SIZE, 0, new Direction("S"), 0, ARENA_SIZE/2 + 1, 0)};
 		Plane[] player2Planes = {new Plane(ARENA_SIZE, 0, new Direction("N"), ARENA_SIZE - 1, ARENA_SIZE/2 + 1, ARENA_SIZE - 1),
 								new Plane(ARENA_SIZE, 0, new Direction("N"), ARENA_SIZE - 1, ARENA_SIZE/2 - 2, ARENA_SIZE - 1)};
-		
+
 		String eol = System.getProperty("line.separator");
 		out.println("The fight between " + player1String + " and " + player2String + " begins and will go on for " + ROUNDS + " rounds." + eol);
-		
+
 		for (int i=ROUNDS - 1; i>=0; i--) {
 			// Announce the round.
 			out.println(eol + "ROUND " + Integer.toString(ROUNDS - i) + eol);
-			
+
 			// Update the plane objects of the players.
 			player1.setPlane1(player1Planes[0].copy());
 			player1.setPlane2(player1Planes[1].copy());
@@ -125,14 +125,14 @@ public class Controller {
 			player2.setPlane2(player2Planes[1].copy());
 			player2.setEnemyPlane1(player1Planes[0].copy());
 			player2.setEnemyPlane2(player1Planes[1].copy());
-			
+
 			// Report the planes' status.
 			out.println(player1String + "'s first plane: " + player1Planes[0].getAsString() + eol);
 			out.println(player1String + "'s second plane: " + player1Planes[1].getAsString() + eol);
 			out.println(player2String + "'s first plane: " + player2Planes[0].getAsString() + eol);
 			out.println(player2String + "'s second plane: " + player2Planes[1].getAsString() + eol);
 			out.flush();
-			
+
 			// Tell the players how many rounds are left.
 			player1.setRoundsLeft(i);
 			player2.setRoundsLeft(i);
@@ -141,8 +141,8 @@ public class Controller {
 			Move[] player1Moves = player1.act();
 			Move[] player2Moves = player2.act();
 
-			
-			
+
+
 			// Check if valid moves were submitted and report them.
 			Move[] moves = new Move[4];
 
@@ -177,7 +177,7 @@ public class Controller {
 				moves[3] = player2Moves[1];
 				out.println(player2String + "'s second plane move: " + player2Moves[1].getAsString() + eol);
 			}
-			
+
 			// Calculate the new positions.
 			player1Planes[0] = player1Planes[0].simulateMove(moves[0]);
 			player1Planes[1] = player1Planes[1].simulateMove(moves[1]);
@@ -197,7 +197,7 @@ public class Controller {
 				out.println(player1String + "'s second plane: " + player1Planes[1].getAsString() + eol);
 				out.println(player2String + "'s first plane: " + player2Planes[0].getAsString() + eol);
 				out.println(player2String + "'s second plane: " + player2Planes[1].getAsString() + eol);
-				
+
 				out.println(eol + "And that's it, both players are out of planes, it's a draw!" + eol);
 				return 0; // Both players are out of planes.
 			}
@@ -207,7 +207,7 @@ public class Controller {
 				out.println(player1String + "'s second plane: " + player1Planes[1].getAsString() + eol);
 				out.println(player2String + "'s first plane: " + player2Planes[0].getAsString() + eol);
 				out.println(player2String + "'s second plane: " + player2Planes[1].getAsString() + eol);
-				
+
 				out.println(eol + "And that's it, " + player1String + " is out of planes, " + player2String + " Wins" + eol);
 				return -1; // Player1 is out of planes.
 			}
@@ -217,12 +217,12 @@ public class Controller {
 				out.println(player1String + "'s second plane: " + player1Planes[1].getAsString() + eol);
 				out.println(player2String + "'s first plane: " + player2Planes[0].getAsString() + eol);
 				out.println(player2String + "'s second plane: " + player2Planes[1].getAsString() + eol);
-				
+
 				out.println(eol + "And that's it, " + player2String + " is out of planes, " + player1String + " Wins" + eol);
 				return 1; // Player2 is out of planes.
 			}			
 		}
-		
+
 		out.println("And that's it, we're out of time" + eol);
 
 		// Report the planes' status.
@@ -230,10 +230,10 @@ public class Controller {
 		out.println(player1String + "'s second plane: " + player1Planes[1].getAsString() + eol);
 		out.println(player2String + "'s first plane: " + player2Planes[0].getAsString() + eol);
 		out.println(player2String + "'s second plane: " + player2Planes[1].getAsString() + eol);
-		
+
 		int winner = ((player1Planes[0].isAlive())?1:0) + ((player1Planes[1].isAlive())?1:0) + 
 						((player2Planes[0].isAlive())?-1:0) + ((player2Planes[1].isAlive())?-1:0);
-		
+
 		if (winner > 0) {
 			out.println(eol + player1String + " wins!" + eol);
 		}
@@ -243,14 +243,14 @@ public class Controller {
 		if (winner == 0) {
 			out.println(eol + "It's a draw!" + eol);
 		}
-		
+
 		return winner;
 	}
 
 	// Check if collisions happened and react appropriately
 	private static void handleCollisions(Plane[] planes, PrintWriter out) {
 		String eol = System.getProperty("line.separator");
-		
+
 		boolean[] collided = new boolean[planes.length];
 		for (int i=0; i<collided.length; i++) {
 			collided[i] = false;
@@ -275,7 +275,7 @@ public class Controller {
 	// Check if anyone got shot and react appropriately.
 	private static void handleShooting(Plane[] planes, Move[] moves, PrintWriter out) {
 		String eol = System.getProperty("line.separator");
-		
+
 		boolean[] hit = new boolean[planes.length];
 		for (int i=0; i<hit.length; i++) {
 			hit[i] = false;
@@ -283,8 +283,6 @@ public class Controller {
 
 		for (int i=0; i<planes.length; i++) {
 			if (moves[i].shoot && planes[i].canShoot()) {
-				planes[i].setCoolDown(COOLDOWN);
-
 				Point3D[] range = planes[i].getShootRange();
 				for (int j=0; j<range.length; j++) {
 					for (int k=0; k<planes.length; k++) {
@@ -302,6 +300,7 @@ public class Controller {
 			if (hit[i]) {
 				planes[i].setAlive(false);
 			}
+			planes[i].updateCoolDown(moves[i].shoot);
 		}
 	}
 }
